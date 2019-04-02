8047d0c31d5eb5c8d61599648a2f0a52e7bd95f0
Oksana <oksanabibik93@gmail.com>
2019-04-02 11:36:50 -0700

    add ft_atoi

--- level_2/ft_atoi/ft_atoi.c
+++ level_2/ft_atoi/ft_atoi.c
@@ -13,3 +13,38 @@ Your function must be declared as follows:
 
 int	ft_atoi(const char *str);
 */
+
+// #include <stdlib.h>
+#include <stdio.h>
+
+int	ft_atoi(const char *str){
+    int res = 0;
+    int negativeNumber = 1;
+
+    if(*str && (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\v' || *str == '\f' || *str == '\r' )){
+        ++str;
+    }
+
+    if(*str == '-'){
+        negativeNumber = -1;
+    }
+    if(*str == '-' || *str == '+'){
+        ++str;
+    }
+
+    while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		++str;
+	}
+
+    return res * negativeNumber;
+}
+
+int main(){
+    char *myStr = "43";
+    printf("%d", ft_atoi(myStr));
+    // printf("%d", atoi(myStr));
+
+    return 0;
+}
