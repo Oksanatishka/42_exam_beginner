0c36a5e901ec7e2261714c0c3e0415d37b03ce2d
Oksana <oksanabibik93@gmail.com>
2019-04-02 13:11:55 -0700

    add ft_strrev

--- level_2/ft_strrev/ft_strrev.c
+++ level_2/ft_strrev/ft_strrev.c
@@ -12,3 +12,36 @@ Your function must be declared as follows:
 
 char    *ft_strrev(char *str);
 */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char    *ft_strrev(char *str){
+    int len=0;
+    int i=0;
+    char temp;
+
+    while(str[len] != '\0')
+        len++;
+    // printf("%d", len);
+    while(i<len-1 ){    
+        temp = str[i];
+        str[i] = str[len-1];
+        str[len-1] = temp;
+        i++;
+        len--;
+    }
+    
+    return str;
+}
+int main(){
+    char myStr[30] = "String abc.";
+    printf("%s", ft_strrev(myStr));
+    return 0;
+}
+
+// str[0] -> str[10]
+// str[1] -> str[9]
+// str[2] -> str[8]
+// str[3] -> str[7]
+// str[4] -> str[6]
