48863f501097d11eefb421f355d3b7e6b5771abd
Oksana <oksanabibik93@gmail.com>
2019-04-02 12:38:20 -0700

    add ft_strdup

--- level_2/ft_strdup/ft_strdup.c
+++ level_2/ft_strdup/ft_strdup.c
@@ -10,3 +10,30 @@ Your function must be declared as follows:
 
 char    *ft_strdup(char *src);
 */
+
+#include <stdio.h>
+// #include <string.h>
+#include <stdlib.h>
+
+char    *ft_strdup(char *src){
+    char *result;
+    int i=0;
+    int len = 0;
+    
+    while(src[len] != '\0'){
+        len++;
+    }
+    result = (char *)malloc(sizeof(*result) * (len +1));
+    while(i<len){
+        result[i] = src[i];
+        i++;
+    }
+    return result;
+}
+
+int main(){
+    char *str = "My String.";
+    printf("%s", ft_strdup(str));
+    // printf("%s", strdup(str));
+    return 0;
+}
