ab05903e63ef0cfcf4d40e225a8df2169a0a1880
Oksana <oksanabibik93@gmail.com>
2019-04-02 11:54:10 -0700

    add ft_strcmp

--- level_2/ft_strcmp/ft_strcmp.c
+++ level_2/ft_strcmp/ft_strcmp.c
@@ -10,3 +10,26 @@ Your function must be declared as follows:
 
 int    ft_strcmp(char *s1, char *s2);
 */
+
+#include <stdio.h>
+#include <string.h>
+
+int    ft_strcmp(char *s1, char *s2){
+    int i=0;
+    while(s1[i] && s2[i]){
+        if(s1[i] != s2[i]){
+            return s1[i] - s2[i];
+        }
+        i++;
+    }
+    return 0;
+}
+
+int main(){
+    char *str1 = "String one";
+    char *str2 = "Ktring one";
+
+    printf("%d", ft_strcmp(str1,str2));
+    // printf("%d", strcmp(str1,str2));
+    return 0;
+}
