72b45e3a8b990de03797596b6f8f0f120d636e0b
Oksana <oksanabibik93@gmail.com>
2019-04-03 17:26:14 -0700

    add add_prime

--- level_3/add_prime_sum/add_prime_sum.c
+++ level_3/add_prime_sum/add_prime_sum.c
@@ -22,3 +22,79 @@ $>./add_prime_sum | cat -e
 0$
 $>
 */
+
+#include <unistd.h>
+
+void ft_putchar(char c){
+    write(1, &c, 1);
+}
+
+int convertStrToInt(char *str){ // atoi
+    int i=0;
+    int num =0;
+    int sign = 1;
+
+    
+    if(!str[i])
+        return 0;
+    
+    while(str[i] == ' ' || str[i] == '\t' ||str[i] == '\n' ||str[i] == '\f' || str[i] == '\v' ||str[i] == '\r')
+        i++;
+    
+    if (str[i] == '-' || str[i] == '+'){
+		if (str[i++] == '-')
+			sign = -1;
+    }
+    while(str[i] >= '0' && str[i] <= '9'){
+        num = (num * 10) + (str[i] - '0');
+        i++;
+    }
+    return (num * sign);
+}
+
+void printNum(int n){
+    char ch;
+    if(n <0){
+        n = -n;
+        ft_putchar('-');
+    }
+    if(n< 10){
+        ch = n + '0';
+        ft_putchar(ch);
+    } else {
+        printNum(n /10);
+        printNum(n % 10);
+    }
+}
+
+int isPrime(int num){
+    int i=2;
+    if(num <= 1){
+        return 0;
+    }
+    while(i <= (num/2)){
+        if(num % i == 0){
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int main(int argc, char **argv){
+    if(argc ==2 ){
+        int sum = 0;
+        int myNum = convertStrToInt(argv[1]);
+        while(myNum > 0){
+            if(isPrime(myNum)){
+                sum += myNum;
+            } 
+            myNum--; 
+        } 
+        printNum(sum);
+    } else {
+        ft_putchar('0');
+    }
+    ft_putchar('\n');
+    return 0;
+}
