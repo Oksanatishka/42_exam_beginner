4ef1c117c71c07edc7eb9036573a107ec912d010
Oksana <oksanabibik93@gmail.com>
2019-05-15 15:43:42 -0700

    add ft_range

--- level_3/ft_range/ft_range.c
+++ level_3/ft_range/ft_range.c
@@ -19,3 +19,36 @@ Examples:
 - With (0, 0) you will return an array containing 0.
 - With (0, -3) you will return an array containing 0, -1, -2 and -3.
 */
+
+#include <stdlib.h>
+
+int *ft_range(int start, int end)
+{
+    int *range;
+    int i = 0;
+
+    if (start > end)
+        range = (int *)malloc(sizeof(int) * (start - end));
+    else
+        range = (int *)malloc(sizeof(int) * (end - start + 1));
+
+    while (start != end)
+    {
+        range[i] = start;
+        start += (start > end) ? -1 : 1;
+        i++;
+    }
+    range[i] = start;
+    return (range);
+}
+
+// #include <stdio.h>
+// int main()
+// {
+//     int *res = ft_range(-1, 2);
+//     printf("%d\n", res[0]);
+//     printf("%d\n", res[1]);
+//     printf("%d\n", res[2]);
+//     printf("%d\n", res[3]);
+//     return 0;
+// }
