8df6fd84808e32eb2749598458e2c24fc6a28328
Oksana <oksanabibik93@gmail.com>
2019-05-15 15:59:04 -0700

    add range/rrange

--- level_3/ft_range/ft_range.c
+++ level_3/ft_range/ft_range.c
@@ -28,7 +28,7 @@ int *ft_range(int start, int end)
     int i = 0;
 
     if (start > end)
-        range = (int *)malloc(sizeof(int) * (start - end));
+        range = (int *)malloc(sizeof(int) * (start - end + 1));
     else
         range = (int *)malloc(sizeof(int) * (end - start + 1));
 
--- level_3/ft_rrange/ft_rrange.c
+++ level_3/ft_rrange/ft_rrange.c
@@ -19,3 +19,36 @@ Examples:
 - With (0, 0) you will return an array containing 0.
 - With (0, -3) you will return an array containing -3, -2, -1 and 0.
 */
+
+#include <stdlib.h>
+
+int *ft_rrange(int start, int end)
+{
+    int i = 0;
+    int *rrange;
+
+    if (start > end)
+        rrange = (int *)malloc(sizeof(int) * (start - end) + 1);
+    else
+        rrange = (int *)malloc(sizeof(int) * (end - start + 1));
+
+    while (start != end)
+    {
+        rrange[i] = end;
+        end -= (start > end ? -1 : 1);
+        i++;
+    }
+    rrange[i] = end;
+    return rrange;
+}
+
+// #include <stdio.h>
+// int main()
+// {
+//     int *res = ft_rrange(1, 3);
+//     printf("%d\n", res[0]);
+//     printf("%d\n", res[1]);
+//     printf("%d\n", res[2]);
+//     // printf("%d\n", res[3]);
+//     return 0;
+// }
