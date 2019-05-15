6b199547710a9f08a837d88b68a08fd3d676b5ce
Oksana <oksanabibik93@gmail.com>
2019-05-15 13:39:02 -0700

    add list_size

--- level_3/ft_list_size/ft_list_size.c
+++ level_3/ft_list_size/ft_list_size.c
@@ -20,3 +20,49 @@ typedef struct    s_list
     void          *data;
 }                 t_list;
 */
+
+#include "ft_list.h"
+
+int ft_list_size(t_list *begin_list)
+{
+    int i = 0;
+    t_list *ptr = begin_list;
+
+    if (begin_list)
+    {
+        while (ptr)
+        {
+            ptr = ptr->next;
+            i++;
+        }
+    }
+    return i;
+}
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main()
+// {
+//     t_list *head = NULL;
+//     t_list *second = NULL;
+//     t_list *third = NULL;
+
+//     // allocate 3 nodes in the heap
+//     head = (t_list *)malloc(sizeof(t_list));
+//     second = (t_list *)malloc(sizeof(t_list));
+//     third = (t_list *)malloc(sizeof(t_list));
+
+//     head->data = 1;
+//     head->next = second;
+
+//     second->data = 2;
+//     second->next = third;
+
+//     third->data = 3;
+//     third->next = NULL;
+
+//     int res = ft_list_size(head);
+//     printf("%d\n", res);
+
+//     return 0;
+// }
