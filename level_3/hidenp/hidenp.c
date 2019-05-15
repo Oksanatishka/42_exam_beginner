4bbd01198d6a2fe087a1655e9ae145dcfa784803
Oksana <oksanabibik93@gmail.com>
2019-05-15 16:43:50 -0700

    add hidenp

--- level_3/hidenp/hidenp.c
+++ level_3/hidenp/hidenp.c
@@ -26,3 +26,49 @@ $>./hidenp | cat -e
 $
 $>
 */
+
+#include <unistd.h>
+
+int ft_strlen(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 3)
+    {
+        int i = 0;
+        int j = 0;
+        int len = 0;
+        while (argv[2][j] != '\0')
+        {
+            if (argv[1][i] == argv[2][j])
+            {
+                len++;
+                i++;
+            }
+            j++;
+        }
+        if (len == ft_strlen(argv[1]))
+        {
+            ft_putchar('1');
+        }
+        else
+        {
+            ft_putchar('0');
+        }
+    }
+    ft_putchar('\n');
+    return 0;
+}
