46583ab51f157df04ed0f74951922d49f0a19c7d
Oksana <oksanabibik93@gmail.com>
2019-05-15 23:45:22 -0700

    add lev_5

--- level_5/brackets/brackets.c
+++ level_5/brackets/brackets.c
@@ -31,3 +31,43 @@ $> ./brackets | cat -e
 $
 $>
 */
+
+#include <unistd.h>
+
+int check_matching(char o, char c)
+{
+    return ((o == '(' && c == ')') ||
+            (o == '[' && c == ']') ||
+            (o == '{' && c == '}'));
+}
+
+int brackets(char *str)
+{
+    char stack[2048];
+    int i = -1;
+
+    while (*str)
+    {
+        if (*str == '(' || *str == '[' || *str == '{')
+            stack[++i] = *str;
+        else if (*str == ')' || *str == ']' || *str == '}')
+        {
+            if (check_matching(stack[i], *str))
+                i--;
+            else
+                return (0);
+        }
+        str++;
+    }
+    return (i == -1 ? 1 : 0);
+}
+
+int main(int argc, char **argv)
+{
+    int i = 0;
+
+    while (++i < argc)
+        brackets(argv[i]) ? write(1, "OK\n", 3) : write(1, "Error\n", 6);
+    i == 1 ? write(1, "\n", 1) : 0;
+    return (0);
+}
--- level_5/ft_itoa_base/ft_itoa_base.c
+++ level_5/ft_itoa_base/ft_itoa_base.c
@@ -20,3 +20,60 @@ Your function must be declared as follows:
 
 char	*ft_itoa_base(int value, int base);
 */
+
+#include <stdlib.h>
+
+int ft_strlen(char *s)
+{
+    int i = 0;
+
+    while (s[i])
+        i++;
+    return (i);
+}
+
+char *ft_strrev(char *s)
+{
+    int i = 0;
+    int k = ft_strlen(s) - 1;
+
+    while (i < k)
+    {
+        s[i] ^= s[k];
+        s[k] ^= s[i];
+        s[i] ^= s[k];
+        i++;
+        k--;
+    }
+    return (s);
+}
+
+char *ft_itoa_base(int value, int base)
+{
+    char *hex = "0123456789ABCDEF";
+    char *total = (char *)malloc(13);
+    int i = 0;
+    int n = 1;
+
+    if (!value)
+    {
+        total[0] = '0';
+        total[1] = '\0';
+        return (total);
+    }
+    if (value < 0)
+        n = -1;
+    while (value)
+    {
+        total[i] = hex[(value * n) % base];
+        value /= base;
+        i++;
+    }
+    if (n == -1 && base == 10)
+    {
+        total[i] = '-';
+        i++;
+    }
+    total[i] = '\0';
+    return (ft_strrev(total));
+}
