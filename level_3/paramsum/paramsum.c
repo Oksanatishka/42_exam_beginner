3b5a4d32519c9a129744799883d33e9c41c2b647
Oksana <oksanabibik93@gmail.com>
2019-05-15 17:25:58 -0700

    add paramsum

--- level_3/paramsum/paramsum.c
+++ level_3/paramsum/paramsum.c
@@ -19,3 +19,24 @@ $>./paramsum | cat -e
 0$
 $>
 */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putnbr(int n)
+{
+    n > 9 ? ft_putnbr(n / 10) : 0;
+    ft_putchar(n % 10 + 48);
+}
+
+int main(int argc, char **argv)
+{
+    // (void)argv;
+    ft_putnbr(argc - 1);
+    write(1, "\n", 1);
+    return (0);
+}
