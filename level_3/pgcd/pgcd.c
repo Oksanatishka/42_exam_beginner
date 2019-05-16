3d7582453656556951308c995663ef25ba443665
Oksana <oksanabibik93@gmail.com>
2019-05-15 17:42:12 -0700

    add pgcd

--- level_3/pgcd/pgcd.c
+++ level_3/pgcd/pgcd.c
@@ -25,3 +25,31 @@ $> ./pgcd 17 3 | cat -e
 $> ./pgcd | cat -e
 $
 */
+
+#include <stdio.h>  // printf
+#include <stdlib.h> // atoi, malloc, free
+
+void pgcd(int nb1, int nb2)
+{
+    int div;
+    int pgcd;
+
+    div = 1;
+    if (nb1 <= 0 || nb2 <= 0)
+        return;
+    while (div <= nb1 || div <= nb2)
+    {
+        if (nb1 % div == 0 && nb2 % div == 0)
+            pgcd = div;
+        div++;
+    }
+    printf("%d", pgcd);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 3)
+        pgcd(atoi(argv[1]), atoi(argv[2]));
+    printf("\n");
+    return (0);
+}
